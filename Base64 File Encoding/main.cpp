4bf1000f79ed3e087beba05fb5e2cc93111fa408
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 14:37:48 +0800

    Initial Commit

---
+++ Base64 File Encoding/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Base64 File Encoding
+//
+//  Created by 56789 01234 on 2024/4/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
