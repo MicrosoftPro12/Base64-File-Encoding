f98cd09db22d92eee2c42c19f0dd52ed023266ef
MicrosoftPro12 <2631352141@qq.com>
2024-07-04 01:37:20 +0800

    Create project.

--- Base64 File Encoding/main.cpp
+++ Base64 File Encoding/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include "base64.h"
+#include <string>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    string a;
+    cout << "将开始Base64编码，请输入你所需要编码的文件名（包括扩展名）：";
+    cin >> a;
+    ifstream is(a, ifstream::in | ios::binary);
+    is.seekg(0, is.end);
+    int length = is.tellg();
+    is.seekg(0, is.beg);
+    
+    char *buffer = new char[length];
+    is.read(buffer, length);
+    string img = aip::base64_encode(buffer, length);
+    cout << img << endl;
+    delete[]buffer;
+    is.close();
     return 0;
 }
